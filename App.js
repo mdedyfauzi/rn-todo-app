b73049ab8ead68d56925a5cfeaa5239b47f42928
Maulana Dedy Fauzi <mdedyfauzi@gmail.com>
2022-11-23 14:49:20 +0700

    working with core components

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Jon!</Text>
+      <Text>Hello Jon ;)</Text>
+      <Button title='Tap me!' />
       <StatusBar style="auto" />
     </View>
   );
