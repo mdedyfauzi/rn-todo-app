291b094ed20ba7dc6703e1b21d649a9ad3daf252
Maulana Dedy Fauzi <mdedyfauzi@gmail.com>
2022-11-23 15:03:13 +0700

    styling

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Jon ;)</Text>
-      <Button title='Tap me!' />
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Assalamualaikum !!!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello Jon ;)</Text>
+      <Button title="Tap me!" />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -14,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });
